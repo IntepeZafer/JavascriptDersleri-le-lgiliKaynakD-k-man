cdb33eaf95f2808d16da7411ea25821a26217e56
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2024-10-20 12:43:11 +0300

    Yeni Ders İçerikleri Eklendi

---
+++ JavascriptGirişVeDataTipleri/Ders3/app.js
@@ -0,0 +1,4 @@
+let gun = 14;
+let ay = 7;
+let yil = 1992;
+console.log("Can Boz " + gun + "/" + ay + "/" + yil + " Tarihinde Doğdu");
---
+++ JavascriptGirişVeDataTipleri/Ders4/app.js
@@ -0,0 +1,7 @@
+let firstName = "Zafer İntepe";
+console.log(firstName);
+console.log(firstName[0]);
+console.log(firstName.length);
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.toUpperCase());
+console.log(firstName.indexOf('Z'));
